4a0dd58a4257127b3a3ab198ad44c3fd461977a6
jennifer-shin <jenshin@usc.edu>
2017-06-21 17:21:17 -0700

    Finished video 5

--- app/src/main/java/com/example/jennifershin/flicks/models/Movie.java
+++ app/src/main/java/com/example/jennifershin/flicks/models/Movie.java
@@ -13,6 +13,7 @@ public class Movie {
     private String title;
     private String overview;
     private String posterPath; // only the path
+    private String backdropPath;
 
     // initialize from JSON data
     public Movie(JSONObject object) {
@@ -20,6 +21,7 @@ public class Movie {
             title = object.getString("title");
             overview = object.getString("overview");
             posterPath = object.getString("poster_path");
+            backdropPath = object.getString("backdrop_path");
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -35,5 +37,9 @@ public class Movie {
 
     public String getPosterPath() {
         return posterPath;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
     }
 }
